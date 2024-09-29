628ebc142591d0b901ed6747c627c7a288b25ff9
Danilo Freire <danilofreire@gmail.com>
2024-09-29 07:42:18 -0400

    add lecture 09

--- lectures/lecture-09/scripts/example_functions.py
+++ lectures/lecture-09/scripts/example_functions.py
@@ -3,7 +3,7 @@ def fn_quadratic(x):
     return(x**2)
 
 def fn_cubic(x):
-    return(x**2)
+    return(x**3)
 
 def message_hello(x):
     return("hi "+ x)
--- lectures/lecture-09/scripts/example_variables.py
+++ lectures/lecture-09/scripts/example_variables.py
@@ -1,4 +1,3 @@
-
 # This file contains 
 
 alpha = 5
